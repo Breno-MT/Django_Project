f74301eb8a3d03ff549f29fafc43cb0e9ff7f935
Breno <hentgenp@gmail.com>
2024-01-16 00:54:41 -0300

    FEAT: added recipes urls to the main app

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls'))  # the URL will be https://domain.com/{URL_IN_RECIPES_URLS_FILE} different from below
+    # path('recipes', include('recipes.urls'))  # every url would become https://domain.com/recipes/{URL_IN_RECIPES_URLS_FILE}
+
 ]
