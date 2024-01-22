45d0936964d86c9e548c84d723c2d34a4e1f8f19
Breno-MT <hentgenp@gmail.com>
2024-01-21 23:38:02 -0300

    REFACT: removed About and Contact from views (VSCODE problem)

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_url(request):
     return render(request, 'recipes/home.html')
 
-
-def contact_url(request):
-    return render(request, 'recipes/contact.html')
-
-
-def about_url(request):
-    return render(request, 'recipes/about.html')
