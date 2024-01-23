466f09853d25d20f42514351755facbcc4b1bcae
Breno-MT <hentgenp@gmail.com>
2024-01-22 23:01:25 -0300

    REFACT: changed template_name to correct path

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home_url(request):
-    return render(request, 'recipes/home.html', context={'name': 'Home'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Home'})
 
