7a4c475ab2acf6de9506c28e502345f8759b417c
Breno-MT <hentgenp@gmail.com>
2024-03-14 02:33:22 -0300

    REFACT: added recipes/recipe on context views.py param to use the info from Faker on both home.html and recipe.html (necessary for 'for loop')

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.faker_info import make_recipe
 
 def home_url(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Home'})
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Home',
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Recipe View'})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Recipe View',
+        'recipe': make_recipe()
+    })
 
