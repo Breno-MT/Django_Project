e4f7a7722baff616e6e44b147ffd8aa4305f2a11
Breno-MT <hentgenp@gmail.com>
2024-06-18 02:24:07 -0300

    FEAT: added MEDIA and STATIC_FILES to the urlpatterns, and imported from django library to make it happen

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))  # the URL will be https://domain.com/{URL_IN_RECIPES_URLS_FILE} different from below
     # path('recipes', include('recipes.urls'))  # every url would become https://domain.com/recipes/{URL_IN_RECIPES_URLS_FILE}
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT
+)
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT
+)
