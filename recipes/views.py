e4ca24d3231bf5f12335a4d2f532b278389cfdd4
Breno-MT <hentgenp@gmail.com>
2024-07-22 21:32:29 -0300

    FEAT: added category return page, fixed recipe() not showing recipe view, changed home to display from database

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from utils.recipes.faker_info import make_recipe
+from recipes.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Home',
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes,
+        # 'recipes': [make_recipe() for _ in range(10)] # In case i need to use in the future.
+    })
+
+def category(request, category_id):
+    category = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Home',
+        'recipes': category,
+        # 'recipes': [make_recipe() for _ in range(10)] # In case i need to use in the future.
     })
 
 def recipe(request, id):
+    recipes = Recipe.objects.filter(id=id).first()
     return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Recipe View',
-        'recipe': make_recipe(),
+        'recipe': recipes,
         'is_viewing_recipe': True,
     })
 
